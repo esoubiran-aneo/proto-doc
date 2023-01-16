3192ef18f13eee91b6e7711757ec5ebf237b3322
esoubiran-aneo <esoubiran@aneo.fr>
2023-01-16 09:52:00 +0100

    docs: add workflow

--- .docs/nuxt.config.ts
+++ .docs/nuxt.config.ts
@@ -1,8 +1,6 @@
-console.log("NODE_ENV", process.env.NODE_ENV);
-
 export default defineNuxtConfig({
   app: {
-    baseUrl: process.env.NODE_ENV === "production" ? "/proto-doc/" : "",
+    baseURL: process.env.NODE_ENV === "production" ? "/proto-doc/" : "",
   },
 
   extends: "@nuxt-themes/docus",
